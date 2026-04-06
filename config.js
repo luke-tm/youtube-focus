f6be7abad8854e8b433631275854250aff9e5282
luke-tm <qwak051502@gmail.com>
2026-04-06 18:24:50 -0400

    Update YouTube CLIENT_ID in config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // Replace YOUR_CLIENT_ID_HERE with your OAuth Client ID
 
 var CONFIG = {
-  CLIENT_ID: 'YOUR_CLIENT_ID_HERE',
+  CLIENT_ID: '83656850398-etlb0jelnhmk5bok4gt7oqr6lkr38f4u.apps.googleusercontent.com',
   REDIRECT_URI: 'https://luke-tm.github.io/youtube-focus/',
   API_BASE: 'https://www.googleapis.com/youtube/v3',
   SCOPES: 'https://www.googleapis.com/auth/youtube.readonly https://www.googleapis.com/auth/youtube https://www.googleapis.com/auth/youtube.force-ssl',
